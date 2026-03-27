cde4d44fdc37fe18d3a22f5808f565ed212f559b
Marco Donadio <marcodonadiocaste@gmail.com>
2026-03-27 10:54:21 -0600

    Definition and creation of the class Interseccion

---
+++ src/main/java/org/example/Interseccion.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * La clase interseccion es la encargada de representar
+ * una interseccion vial dentro de la simulacion de
+ * la red urbana.
+ *
+ * Podemos verla como la entidad principal a indexar en
+ * el BST y AVL
+ *
+ * Implementa de una el comparable para el ordenamiento
+ * por ID (a implementarse y evaluarse claro)
+ */
+public class Interseccion {
+}
